8e73e44eaace5c22f237cd96954d118d2e8e6d19
j-lia <jul.y.zhe@gmail.com>
2022-09-20 02:24:18 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
+import bg from './home-bg.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={bg} alt="bg" className="bg"/>
+      <link rel="stylesheet" href="https://use.typekit.net/kar8wbl.css"></link>
+      <span className="text" style={{left: '130px', top: '110px'}}>
+        a slight net cast across the stream of perception—
+      </span>
+      <span className="text" style={{left: '635px', top: '425px'}}>
+        —this is that.
+      </span>
+      <span className="text" style={{fontStyle: 'italic', color: 'rgb(251,0,118)', left: '825px', top: '425px'}}>
+        IN PROGRESS.
+      </span>
     </div>
   );
 }
-
 export default App;
