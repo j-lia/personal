8d068e911c3851ed908fdea9bb7ba255d3e22cf5
j-lia <jul.y.zhe@gmail.com>
2022-09-23 01:39:51 -0400

    design #2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 
 import bg from './img/home-bg.png';
-import decal1 from './img/decal1.png';
-import decal2 from './img/decal2.png';
 import './App.css';
 
 function App() {
   return (
-    <div style={{ height: '100vh' }}>
-      <img src={decal1} alt="decal1" className="bg" style={{ top: '-53px', left: '14px', width: '174px', overflow: '205.36px' }}/>
-      <img src={decal2} alt="decal2" className="bg" style={{ top: '71px', left: '425px', width: '1512px', overflow: 'hidden' }}/>
+    <div>
       <div style={{ marginLeft: '180px', marginTop: '120px'}}>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Gafata"></link>
-        <span className="title">
-          Julia Zheng
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Barlow"></link>
+        <span className="title" style={{ width: '200px' }}>
+          julia zheng
         </span>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <div style={{ display: 'inline-block' }}>
+        <div style={{ display: 'inline-block', marginLeft: '72px' }}>
           <button className="btn">&nbsp;&nbsp;WORDS&nbsp;&nbsp;</button>
-          <button className="btn">&nbsp;&nbsp;PICTURES&nbsp;&nbsp;</button>
+          <button className="btn">&nbsp;&nbsp;1 PICTURE&nbsp;&nbsp;</button>
         </div>
-        <div>
-          <img src={bg} alt="bg" className="bg" style={{ top: '200px', width: '725px' }}/>
-          <link rel="stylesheet" href="https://use.typekit.net/kar8wbl.css"></link>
-          <span className="text" style={{ left: '250px', top: '250px', color: 'white' }}>
-            a slight net cast across the stream of perception—
-          </span>
-          <span className="text" style={{ left: '780px', top: '575px', color: 'white' }}>
-            —this is that.
-          </span>
+        <div className='box' style={{ backgroundImage: `url(${bg})` }}>
+          is a Yale senior thinking about
+          <ul>
+            <li>algorithmic alignment,</li>
+            <li>in silico models of the brain, and</li>
+            <li>Lindt Intense Orange Dark Chocolate.</li>
+          </ul>
+          She is currently
+          <ul>
+            <li>listening to Sandy Lam,</li>
+            <li>reading <em>The Turnaway Study</em>, and</li>
+            <li>learning to climb rocks.</li>
+          </ul>
+          <br></br>
+          <a className='link' href = "mailto: julia.zheng@yale.edu">EMAIL↗</a>&nbsp;&nbsp;
+          <a className='link' href = "https://www.linkedin.com/in/julia-y-zheng/">LINKEDIN↗</a>&nbsp;&nbsp;
+          <a className='link' href = "https://www.instagram.com/juliaxyzz/">INSTAGRAM↗</a>
         </div>
       </div>
     </div>
